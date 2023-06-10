7c24df1a92b603a557a793335a96ecb6c6fc153c
Kerem <keremkys16@gmail.com>
2023-06-10 10:57:45 +0300

    add push method to navigation and add url launcher

--- lib/core/init/network/network_service.dart
+++ lib/core/init/network/network_service.dart
@@ -1,10 +1,10 @@
+import 'package:steam_deals_application/core/constants/api/api_constants.dart';
 import 'package:steam_deals_application/core/enum/network/api_path.dart';
 import 'package:vexana/vexana.dart';
 
 class NetworkService<T extends INetworkModel<T>> {
   NetworkManager<T> _getService() {
-    final baseOptions =
-        BaseOptions(baseUrl: 'https://www.cheapshark.com/api/1.0');
+    final baseOptions = BaseOptions(baseUrl: ApiContants.cheapsharkApiBaseUrl);
     return NetworkManager<T>(options: baseOptions);
   }
 
@@ -17,5 +17,21 @@ class NetworkService<T extends INetworkModel<T>> {
     );
 
     return response.data;
+  }
+
+  Future<R?> fetchLookup<R>(
+    ApiPath path,
+    T parseModel,
+    String id,
+  ) async {
+    assert(
+      path == ApiPath.dealWithId,
+      'fetchLookup only works with id parameter',
+    );
+    final response = await _manager.get<R>(
+      path.getWithIdPath(id),
+    );
+    final body = response.data;
+    return body;
   }
 }
